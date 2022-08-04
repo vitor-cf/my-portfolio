42f874c2a5c2164fac09af442460fc88324f87a4
Vitor <vitorcarvalho.dev@gmail.com>
2022-08-03 22:28:05 -0300

    atualizando portfolio

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,307 @@
 // Função Dark-mode
 
-function darkMode () {
-    let luz = document.getElementById('lua');
-    luz.style.display = 'block'; 
-    let off = document.getElementById('sol');
-    off.style.display = 'none';  
+// function darkMode () {
+//     let luz = document.getElementById('lua');
+//     luz.style.display = 'block'; 
+//     let off = document.getElementById('sol');
+//     off.style.display = 'none';  
 
-    var element = document.body;
-    element.style.background = 'rgb(5, 5, 36)'
-} 
+//     var element = document.body;
+//     element.style.background = 'rgb(5, 5, 36)'
+// } 
 
-function lightMode () {
+// function lightMode () {
 
-    let luz = document.getElementById('lua');
-    luz.style.display = 'none'; 
-    let off = document.getElementById('sol');
-    off.style.display = 'block'; 
+//     let luz = document.getElementById('lua');
+//     luz.style.display = 'none'; 
+//     let off = document.getElementById('sol');
+//     off.style.display = 'block'; 
 
-    var element = document.body;
-    element.style.background = 'rgb(255, 255, 255)'
-} 
+//     var element = document.body;
+//     element.style.background = 'rgb(255, 255, 255)'
+// } 
     
-const sol = document.getElementById('sol');
-sol.addEventListener('click', darkMode); 
+// const sol = document.getElementById('sol');
+// sol.addEventListener('click', darkMode); 
 
-const lua = document.getElementById('lua');
-lua.addEventListener('click', lightMode )
+// const lua = document.getElementById('lua');
+// lua.addEventListener('click', lightMode )
     
     
+// mudar imagem tools 
+
+// HTML
+
+function mudaImagemColorida () {
     
+    let noColor = document.getElementById('iconNotColor');
+    noColor.style.display = 'none';
+
+    let yesColor = document.getElementById('iconColor');
+    yesColor.style.display = 'block';
+}  
+
+function returnIcon () {
+
+    let noColor = document.getElementById('iconNotColor');
+    noColor.style.display = 'block';
+
+    let yesColor = document.getElementById('iconColor');
+    yesColor.style.display = 'none';
+
+}
 
 
+const iconPass = document.getElementById('iconNotColor');
+iconPass.addEventListener('mouseover', mudaImagemColorida); 
+
+const teste = document.getElementById('iconColor');
+teste.addEventListener('mouseout', returnIcon);
 
 
+// CSS 
+
+function mudaImagemColoridaCss () {
+    
+    let noColor = document.getElementById('iconNotColor-css');
+    noColor.style.display = 'none';
+
+    let yesColor = document.getElementById('iconColor-css');
+    yesColor.style.display = 'block';
+}  
+
+function returnIconCss () {
+
+    let noColor = document.getElementById('iconNotColor-css');
+    noColor.style.display = 'block';
+
+    let yesColor = document.getElementById('iconColor-css');
+    yesColor.style.display = 'none';
+
+}
 
 
+const cssClick = document.getElementById('iconNotColor-css');
+cssClick.addEventListener('mouseover', mudaImagemColoridaCss); 
+
+const testeCss = document.getElementById('iconColor-css');
+testeCss.addEventListener('mouseout', returnIconCss); 
+
+// JS 
+
+function mudaImagemColoridaJs () {
+    
+    let noColor = document.getElementById('iconNotColor-js');
+    noColor.style.display = 'none';
+
+    let yesColor = document.getElementById('iconColor-js');
+    yesColor.style.display = 'block';
+}  
+
+function returnIconJs () {
+
+    let noColor = document.getElementById('iconNotColor-js');
+    noColor.style.display = 'block';
+
+    let yesColor = document.getElementById('iconColor-js');
+    yesColor.style.display = 'none';
+
+}
+
+
+const jsClick = document.getElementById('iconNotColor-js');
+jsClick.addEventListener('mouseover', mudaImagemColoridaJs); 
+
+const testeJs = document.getElementById('iconColor-js');
+testeJs.addEventListener('mouseout', returnIconJs);
+
+// react 
+
+function mudaImagemColoridaReact () {
+    
+    let noColor = document.getElementById('iconNotColor-react');
+    noColor.style.display = 'none';
+
+    let yesColor = document.getElementById('iconColor-react');
+    yesColor.style.display = 'block';
+}  
+
+function returnIconReact () {
+
+    let noColor = document.getElementById('iconNotColor-react');
+    noColor.style.display = 'block';
+
+    let yesColor = document.getElementById('iconColor-react');
+    yesColor.style.display = 'none';
+
+}
+
+
+const reactClick = document.getElementById('iconNotColor-react');
+reactClick.addEventListener('mouseover', mudaImagemColoridaReact); 
+
+const testeReact = document.getElementById('iconColor-react');
+testeReact.addEventListener('mouseout', returnIconReact);
+
+// vscode 
+function mudaImagemColoridaVscode () {
+    
+    let noColor = document.getElementById('iconNotColor-vs');
+    noColor.style.display = 'none';
+
+    let yesColor = document.getElementById('iconColor-vs');
+    yesColor.style.display = 'block';
+}  
+
+function returnIconVscode () {
+
+    let noColor = document.getElementById('iconNotColor-vs');
+    noColor.style.display = 'block';
+
+    let yesColor = document.getElementById('iconColor-vs');
+    yesColor.style.display = 'none';
+
+}
+
+
+const vsClick = document.getElementById('iconNotColor-vs');
+vsClick.addEventListener('mouseover', mudaImagemColoridaVscode); 
+
+const testeVs = document.getElementById('iconColor-vs');
+testeVs.addEventListener('mouseout', returnIconVscode);
+
+// git 
+function mudaImagemColoridaGit () {
+    
+    let noColor = document.getElementById('iconNotColor-git');
+    noColor.style.display = 'none';
+
+    let yesColor = document.getElementById('iconColor-git');
+    yesColor.style.display = 'block';
+}  
+
+function returnIconGit () {
+
+    let noColor = document.getElementById('iconNotColor-git');
+    noColor.style.display = 'block';
+
+    let yesColor = document.getElementById('iconColor-git');
+    yesColor.style.display = 'none';
+
+}
+
+
+const gitClick = document.getElementById('iconNotColor-git');
+gitClick.addEventListener('mouseover', mudaImagemColoridaGit); 
+
+const testeGit = document.getElementById('iconColor-git');
+testeGit.addEventListener('mouseout', returnIconGit);
+
+// github 
+function mudaImagemColoridaGithub () {
+    
+    let noColor = document.getElementById('iconNotColor-github');
+    noColor.style.display = 'none';
+
+    let yesColor = document.getElementById('iconColor-github');
+    yesColor.style.display = 'block';
+}  
+
+function returnIconGithub () {
+
+    let noColor = document.getElementById('iconNotColor-github');
+    noColor.style.display = 'block';
+
+    let yesColor = document.getElementById('iconColor-github');
+    yesColor.style.display = 'none';
+
+}
+
+
+const githubClick = document.getElementById('iconNotColor-github');
+githubClick.addEventListener('mouseover', mudaImagemColoridaGithub); 
+
+const testeGithub = document.getElementById('iconColor-github');
+testeGithub.addEventListener('mouseout', returnIconGithub);
+
+// photoshop 
+function mudaImagemColoridaPs () {
+    
+    let noColor = document.getElementById('iconNotColor-ps');
+    noColor.style.display = 'none';
+
+    let yesColor = document.getElementById('iconColor-ps');
+    yesColor.style.display = 'block';
+}  
+
+function returnIconPs () {
+
+    let noColor = document.getElementById('iconNotColor-ps');
+    noColor.style.display = 'block';
+
+    let yesColor = document.getElementById('iconColor-ps');
+    yesColor.style.display = 'none';
+
+}
+
+
+const psClick = document.getElementById('iconNotColor-ps');
+psClick.addEventListener('mouseover', mudaImagemColoridaPs); 
+
+const testePs = document.getElementById('iconColor-ps');
+testePs.addEventListener('mouseout', returnIconPs);
+
+// netlify 
+function mudaImagemColoridaNet () {
+    
+    let noColor = document.getElementById('iconNotColor-net');
+    noColor.style.display = 'none';
+
+    let yesColor = document.getElementById('iconColor-net');
+    yesColor.style.display = 'block';
+}  
+
+function returnIconNet () {
+
+    let noColor = document.getElementById('iconNotColor-net');
+    noColor.style.display = 'block';
+
+    let yesColor = document.getElementById('iconColor-net');
+    yesColor.style.display = 'none';
+
+}
+
+
+const netClick = document.getElementById('iconNotColor-net');
+netClick.addEventListener('mouseover', mudaImagemColoridaNet); 
+
+const testeNet = document.getElementById('iconColor-net');
+testeNet.addEventListener('mouseout', returnIconNet);
+
+// typescript
+
+function mudaImagemColoridaTs () {
+    
+    let noColor = document.getElementById('iconNotColor-ts');
+    noColor.style.display = 'none';
+
+    let yesColor = document.getElementById('iconColor-ts');
+    yesColor.style.display = 'block';
+}  
+
+function returnIconTs () {
+
+    let noColor = document.getElementById('iconNotColor-ts');
+    noColor.style.display = 'block';
+
+    let yesColor = document.getElementById('iconColor-ts');
+    yesColor.style.display = 'none';
+
+}
+
+
+const tsClick = document.getElementById('iconNotColor-ts');
+tsClick.addEventListener('mouseover', mudaImagemColoridaTs); 
+
+const testeTs = document.getElementById('iconColor-ts');
+testeTs.addEventListener('mouseout', returnIconTs);
